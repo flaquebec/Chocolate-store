a58c789edca1bfb10e83f0c4e453da1fd5f14cf6
flaquebec <46978782+flaquebec@users.noreply.github.com>
2020-04-23 00:19:27 -0400

    Primeiro Commit

--- admin/index.php
+++ admin/index.php
@@ -17,13 +17,46 @@ foreach ($admins as $admin) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/assets/css/bootstrap.css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style_chocolate.css">
+    
+    <!-- Fonts-->
+    <<link href="https://fonts.googleapis.com/css?family=Comic+Neue:400,700&display=swap" rel="stylesheet">
+      <!--font-family: 'Comic Neue', cursive; -->
+    
     <title>Admin</title>
   </head>
+ 
   <body>
+  
+  
+  <div class="container-fluid col-xs-12 col-sm-6 col-md-6 col-lg-4">
+    <div id="logoLogin"  class="mx-auto d-block">
+      
+        <img src="imagem/logo.jpg"  id="logoImage"class="mx-auto d-block">
+       
+    </div>
+    <div class="admin ">
+      <p class="text-center">Administrator</p>
+    </div>
+   
+    <form action="/action_page.php">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Password:</label>
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+      </div>
+     
+      <button id="btnLogin" type="button" class="btn btn-primary btn-block">Login</button>
+    </form>
+  </div>
+  
+
+
     
-    <h1>Admin</h1>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
