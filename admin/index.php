3429c39d473f9e66d8d9cff8bb6eb629fc2b1772
flaquebec <46978782+flaquebec@users.noreply.github.com>
2020-04-23 21:36:44 -0400

    First_Task_Done

--- admin/index.php
+++ admin/index.php
@@ -18,10 +18,11 @@ foreach ($admins as $admin) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style_chocolate.css">
+    <link rel="stylesheet" href="cssTest.css">
+  
     
     <!-- Fonts-->
-    <<link href="https://fonts.googleapis.com/css?family=Comic+Neue:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comic+Neue:400,700&display=swap" rel="stylesheet">
       <!--font-family: 'Comic Neue', cursive; -->
     
     <title>Admin</title>
@@ -29,8 +30,10 @@ foreach ($admins as $admin) {
  
   <body>
   
-  
-  <div class="container-fluid col-xs-12 col-sm-6 col-md-6 col-lg-4">
+    <div class="opsMessage alert alert-danger container-fluid text-center d-none col-xs-12 col-sm-6 col-md-6 col-lg-4">
+      <strong>Ops!</strong> Email and/or password invalid.
+    </div>
+  <div class="container-fluid loginBox col-xs-12 col-sm-6 col-md-6 col-lg-4">
     <div id="logoLogin"  class="mx-auto d-block">
       
         <img src="imagem/logo.jpg"  id="logoImage"class="mx-auto d-block">
@@ -43,19 +46,19 @@ foreach ($admins as $admin) {
     <form action="/action_page.php">
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        <input type="email" class="form-control" id="email" value="" placeholder="Enter email" name="email">
       </div>
       <div class="form-group">
         <label for="pwd">Password:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+        <input type="password" class="form-control" id="pwd" value="" placeholder="Enter password" name="pswd">
       </div>
      
-      <button id="btnLogin" type="button" class="btn btn-primary btn-block">Login</button>
+      <button onclick="emailValidation()" id="btnLogin" type="button" class="btn btn-primary btn-block">Login</button>
     </form>
   </div>
   
 
-
+  
     
 
     <!-- Optional JavaScript -->
@@ -63,5 +66,6 @@ foreach ($admins as $admin) {
     <script src="/assets/js/jquery-3.4.1.slim.js"></script>
     <script src="/assets/js/popper.js"></script>
     <script src="/assets/js/bootstrap.js"></script>
+   
   </body>
 </html>
