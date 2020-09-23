ee5d4424c324834796ee0aa2300d3a9369e59b59
Rodrigo Luengo <rpl@outlook.com>
2020-09-22 23:49:35 -0300

    Home

--- index.php
+++ index.php
@@ -5,6 +5,23 @@ $db = new DB();
 
 $categories = $db->query("SELECT id, name, description FROM categories WHERE status = 1 ORDER BY name")->get();
 
+$sql = '
+        SELECT 
+          p.id, 
+          p.name, 
+          p.description,
+          p.price,
+          p.discount,
+          pi.image
+        FROM products AS p
+          INNER JOIN product_images AS pi ON p.id = pi.product_id 
+          WHERE p.status = 1
+          AND pi.position = 0
+          ORDER BY RAND() LIMIT 8
+';
+
+$products = $db->query($sql)->get();
+
 include 'header.php';
 ?>
 
@@ -46,6 +63,25 @@ include 'header.php';
 
 
 <div class="containerPrincipal container-fluid d-flex flex-wrap">
+<?php foreach ($products as $product):?>
+  <div class="boxProduto col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-6">
+      <div class="itemImagemContainer">
+          <a href="/product_detail.php?id=<?php echo $product['id'] ?>">
+            <img src="/assets/img/<?php echo $product['image'] ?>" class="img-fluid itemImage">
+          </a>
+      </div>
+      <div class="itemInfoContainer col-12 p-0">
+          <div class="itemInfo pr-1 pl-1">
+              <p class="itemName"><?php echo $product['name'] ?></p>
+              <?php if ($product['discount']): ?>
+              <p class="itemProm">Preço com desconto</p>
+              <?php endif ?>
+              <p class="itemPrice">R$ <?php echo number_format($product['price'], 2, ',', '.') ?></p>
+              <p class="itemLink"><a href="/product_detail.php?id=<?php echo $product['id'] ?>"> Leia mais</a></p>
+          </div>                 
+      </div>
+  </div>
+<?php endforeach ?>
 </div>
 
 <?php 
